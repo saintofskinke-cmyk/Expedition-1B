99246e4984321af3dd8fd332c31380efaf632a5e
Simon Gade <simongade@duck.com>
2026-03-02 15:47:14 +0100

    Player Prefab

---
+++ Assets/Scripts/PlayerScripts/PlayerController.cs
@@ -0,0 +1,125 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Player Components")]
+    private CharacterController controller;
+    [SerializeField] private Transform eyes;
+    private float eyesPosY;
+    private float eyesPosCrouch, eyesPosStand;
+    [SerializeField] private PlayerLook PlayerLook;
+
+    [Header("Movement Parameters")]
+    private float moveSpeed;
+    private float walkSpeed = 7f;
+    private float sprintSpeed = 12f;
+    private float crouchSpeed = 3.5f;
+    private float jumpForce = 1.5f;
+    private float gravity = -9.81f;
+    private int standingHeight = 2;
+    private int crouchingHeight = 1;
+    private bool isPlayerGrounded;
+    private bool isPlayerCrouching;
+    private Vector3 velocity;
+
+    #region Input Actions
+    [Header("Input Actions")]
+    [SerializeField] private InputActionReference moveAction;
+    [SerializeField] private InputActionReference sprintAction;
+    [SerializeField] private InputActionReference crouchAction;
+    [SerializeField] private InputActionReference jumpAction;
+
+    private void OnEnable()
+    {
+        moveAction.action.Enable();
+        sprintAction.action.Enable();
+        crouchAction.action.Enable();
+        jumpAction.action.Enable();
+    }
+    private void OnDisable()
+    {
+        moveAction.action.Disable();
+        sprintAction.action.Disable();
+        crouchAction.action.Disable();
+        jumpAction.action.Disable();
+    }
+    #endregion
+
+    private void Awake()
+    {
+        controller = GetComponent<CharacterController>();
+        eyesPosY = eyes.position.y;
+        eyesPosCrouch = eyesPosY - 0.3f;
+        eyesPosStand = eyesPosCrouch + 0.3f;
+    }
+
+    private void Update()
+    {
+        // Apply gravity
+        velocity.y += gravity * Time.deltaTime;
+
+        MoveUpdate();
+    }
+
+    private void MoveUpdate()
+    {
+        // Jump
+        isPlayerGrounded = controller.isGrounded;
+        if (isPlayerGrounded && velocity.y < 0f)
+            { velocity.y = -2f; }
+
+        if (jumpAction.action.triggered && isPlayerGrounded)
+            { velocity.y = Mathf.Sqrt(jumpForce * -gravity); }
+
+
+        // Move direction
+        Vector3 eyesForward = eyes.forward;
+        Vector3 eyesRight = eyes.right;
+        eyesForward.y = 0f;
+        eyesRight.x = 0f;
+
+        // Movement
+        Vector2 input = moveAction.action.ReadValue<Vector2>();
+        Vector3 move = eyesRight * input.x + eyesForward * input.y;
+        move.Normalize();
+        if (move != Vector3.zero) { transform.position = move; }
+
+
+        // Crouch
+        if (crouchAction.action.IsPressed() && isPlayerGrounded)
+        {
+            isPlayerCrouching = true;
+            controller.height = crouchingHeight;
+        }
+        else if (isPlayerCrouching && !crouchAction.action.IsPressed())
+        {
+            if (Physics.Raycast(eyes.position, Vector3.up, 0.5f))
+            {
+                isPlayerCrouching = true;
+                controller.height = crouchingHeight;
+            }
+            else
+            {
+                isPlayerCrouching = false;
+                controller.height = standingHeight;
+            }
+            Debug.Log(Physics.Raycast(eyes.position, Vector3.up, 0.5f));
+        }
+
+
+
+        // Apply moveSpeed
+        if (isPlayerGrounded)
+        {
+            if (sprintAction.action.IsPressed()) { moveSpeed = sprintSpeed; }
+            else if (isPlayerCrouching) { moveSpeed = crouchSpeed; }
+            else { moveSpeed = walkSpeed; }
+        }
+
+        // Make MoveUpdate() work
+        Vector3 finalMove = move * moveSpeed + velocity.y * Vector3.up;
+        controller.Move(finalMove * Time.deltaTime);
+    }
+
+}
---
+++ Assets/Scripts/PlayerScripts/PlayerLook.cs
@@ -0,0 +1,112 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
+
+public class PlayerLook : MonoBehaviour
+{
+    [Header("Actions")]
+    [SerializeField] private InputActionReference lookAction;
+    [SerializeField] private InputActionReference handInterAction;
+
+    [Header("Looking Parameters")]
+    [SerializeField] private Transform orientation;
+    private float mouseSens = 20f;
+    private float xRotation;
+    private float yRotation;
+
+    [Header("Pick Up Parameters")]
+    private int pickUpRange = 4;
+    private bool hasItemInHand;
+    [SerializeField] private Transform handAnchor;
+    private Transform originalHandItemAnchor;
+    private Transform itemInHand;
+    private VisualElement root;
+    private Label txtPickUp;
+
+    private void Awake()
+    {
+        UnityEngine.Cursor.lockState = CursorLockMode.Locked;
+        UnityEngine.Cursor.visible = false;
+        root = GetComponent<UIDocument>().rootVisualElement;
+        txtPickUp = root.Q<Label>("txtPickUp");
+    }
+
+    private void OnEnable()
+    {
+        lookAction.action.Enable();
+        handInterAction.action.Enable();
+    }
+
+    private void OnDisable()
+    {
+        lookAction.action.Disable();
+        handInterAction.action.Disable();
+    }
+
+    private void Update()
+    {
+        LookUpdate();
+        PickUpdate();
+    }
+
+    private void LookUpdate()
+    {
+        Vector2 mouse = mouseSens * Time.deltaTime * lookAction.action.ReadValue<Vector2>();
+
+        yRotation += mouse.x;
+        xRotation -= mouse.y;
+        xRotation = Mathf.Clamp(xRotation, -89f, 89f);
+
+        // Camera rotation & orientation
+        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0f);
+        orientation.rotation = Quaternion.Euler(0f, yRotation, 0f);
+    }
+
+    private void PickUpdate()
+    {
+
+        if (Physics.Raycast(Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue()), out RaycastHit hit, pickUpRange)
+            && hit.collider.gameObject.CompareTag("Item"))
+        {
+            txtPickUp.style.display = DisplayStyle.Flex;
+            if (handInterAction.action.WasPressedThisFrame() && !hasItemInHand)
+            {
+                hit.transform.GetComponentInChildren<UIDocument>().enabled = false;
+                originalHandItemAnchor = hit.transform.parent;
+                OnItemPickedUp(hit, handAnchor);
+                itemInHand = hit.transform;
+                hasItemInHand = true;
+            }
+            else TryItemDrop();
+        }
+        else 
+        {
+            TryItemDrop();
+            txtPickUp.style.display = DisplayStyle.None;
+        }
+    }
+    
+    private void TryItemDrop()
+    {
+        if (handInterAction.action.WasPressedThisFrame() && hasItemInHand)
+        {
+            OnItemDropped(hasItemInHand, itemInHand, originalHandItemAnchor);
+            hasItemInHand = false;
+        }
+    }
+    private void OnItemPickedUp(RaycastHit hit, Transform handAnchor)
+    {
+        hit.transform.SetParent(handAnchor);
+        hit.rigidbody.isKinematic = true;
+        hit.collider.enabled = false;
+        hit.transform.position = handAnchor.position;
+    }
+
+    private void OnItemDropped(bool hasItemInHand, Transform itemInHand, Transform originalAnchor)
+    {
+        itemInHand.GetComponent<Rigidbody>().isKinematic = false;
+        itemInHand.GetComponent<Collider>().enabled = true;
+        itemInHand.SetParent(originalAnchor);
+        itemInHand.GetComponentInChildren<UIDocument>().enabled = true;
+    }
+}
