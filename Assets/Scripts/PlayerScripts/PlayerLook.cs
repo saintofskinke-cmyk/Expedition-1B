1dea1504783c6b5a0676bdb18307fd8864cd01fe
Simon Gade <simongade@duck.com>
2026-03-06 20:08:52 +0100

    Camera 1957 Update

--- Assets/Scripts/PlayerScripts/PlayerLook.cs
+++ Assets/Scripts/PlayerScripts/PlayerLook.cs
@@ -90,6 +90,7 @@ public class PlayerLook : MonoBehaviour
                     }
                     return;
                 }
+                TryItemDrop();
                 return;
             }
         }
