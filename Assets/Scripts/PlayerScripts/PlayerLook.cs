cd8c2c9ded6d14df7e009493be0b0bf722222101
Simon Gade <simongade@duck.com>
2026-03-14 02:29:45 +0100

    Lighting

--- Assets/Scripts/PlayerScripts/PlayerLook.cs
+++ Assets/Scripts/PlayerScripts/PlayerLook.cs
@@ -17,6 +17,7 @@ public class PlayerLook : MonoBehaviour
     private float yRotation;
 
     [Header("Pick Up Parameters")]
+    [SerializeField] private Camera raycastCamera;
     private int pickUpRange = 4;
     public bool hasItemInHand;
     [SerializeField] private Transform handAnchor;
@@ -73,9 +74,10 @@ public class PlayerLook : MonoBehaviour
     {
         if (!playerController.isCameraInHand)
         {
-            if (Physics.Raycast(Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue()), out RaycastHit hit, pickUpRange))
+            if (Physics.Raycast(raycastCamera.ScreenPointToRay(Mouse.current.position.ReadValue()), out RaycastHit hit, pickUpRange))
             {
-                if (hit.collider.gameObject.CompareTag("Interactable") || hit.collider.gameObject.CompareTag("Item") || hit.collider.gameObject.CompareTag("Camera") || hit.collider.gameObject.CompareTag("ImportantItem"))
+                GameObject hitObj = hit.collider.gameObject;
+                if (hitObj.CompareTag("Interactable") || hitObj.CompareTag("Item") || hitObj.CompareTag("Camera") || hitObj.CompareTag("ImportantItem") || hitObj.CompareTag("Flare"))
                 {
                     txtPickUp.style.display = DisplayStyle.Flex;
                     if (handInterAction.action.WasPressedThisFrame() && !hasItemInHand)
@@ -87,6 +89,14 @@ public class PlayerLook : MonoBehaviour
                                 OnItemPickedUp(hit, handAnchor);
                                 itemInHand = hit.transform;
                                 hasItemInHand = true;
+                                break;
+
+                            case "Flare":
+                                originalHandItemAnchor = hit.transform.parent;
+                                OnItemPickedUp(hit, handAnchor);
+                                itemInHand = hit.transform;
+                                hasItemInHand = true;
+                                itemInHand.GetComponentInChildren<Light>().transform.localPosition = new Vector3(-0.02f, 0.7f, -0.11f); // Change the position of the light to make it look better in the player's hand
                                 break;
 
                             case "Interactable":
@@ -120,6 +130,10 @@ public class PlayerLook : MonoBehaviour
     {
         if (handInterAction.action.WasPressedThisFrame() && hasItemInHand)
         {
+            if (itemInHand.CompareTag("Flare"))
+            {
+                itemInHand.GetComponentInChildren<Light>().transform.localPosition = new Vector3(0f, 0.92f, 0f); // Change the position of the light to make it look better when dropped
+            }
             OnItemDropped(hasItemInHand, itemInHand, originalHandItemAnchor, 10f);
             hasItemInHand = false;
         }
