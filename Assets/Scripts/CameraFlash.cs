b931437cfba33c963bec4b9ee75c6b17add3b256
Axel Bernhard Dybdal Olin <axelolin5@gmail.com>
2026-03-17 10:41:53 +0100

    changed temp and intensity of flash

--- Assets/Scripts/CameraFlash.cs
+++ Assets/Scripts/CameraFlash.cs
@@ -41,7 +41,7 @@ public class CameraFlash : MonoBehaviour
 
         flash.style.opacity = 100f;
 
-        flashObject.GetComponent<Light>().intensity = 7f;
+        flashObject.GetComponent<Light>().intensity = 2f;
 
         yield return new WaitForSeconds(0.05f);
 
