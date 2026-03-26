f57f9f8e333dbb01a5c7f448552a1212b3701e11
Axel Bernhard Dybdal Olin <axelolin5@gmail.com>
2026-03-26 11:00:46 +0100

    Merge branch 'ObjectiveFinishAxel' into MergeBeforeMain

---
+++ Assets/Scripts/InteriorScripts/PaperInput.cs
@@ -0,0 +1,61 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
+
+public class PaperInput : MonoBehaviour
+{
+    public PlayerController controller;
+    public PlayerLook playerLook;
+    public QuestManager questManager;
+    public Inventory inventory;
+    public InputAction exitUI;
+    
+    private UIDocument paperUIDocument;
+    public UIDocument playerUIDocument;
+    private VisualElement paperUI;
+    private bool showingPaper = false;
+
+    private void Update()
+    {
+        PaperUpdate();
+    }
+
+    private void UpdateUI()
+    {
+        paperUIDocument = gameObject.GetComponent<UIDocument>();
+        paperUI = paperUIDocument.rootVisualElement.Q<VisualElement>("Paper");
+        paperUI.style.display = DisplayStyle.None;
+
+    }
+
+    public void ShowPaperText()
+    {
+        UpdateUI();
+        showingPaper = true;
+        paperUI.style.display = DisplayStyle.Flex;
+    }
+
+    private void PaperUpdate()
+    {
+        if (showingPaper)
+        {
+            controller.enabled = false;
+            playerLook.enabled = false;
+            playerUIDocument.enabled = false;
+            
+            if(GameManager.Instance.exitUIAction.action.WasPressedThisFrame())
+            {
+                paperUI.style.display = DisplayStyle.None;
+                showingPaper = false;
+                controller.enabled = true;
+                playerLook.enabled = true;
+
+                playerUIDocument.enabled = true;
+
+                inventory.UpdatePlayerHud();
+                questManager.UpdateUI();
+                playerLook.UpdateTextUI();
+            }
+        }
+    }
+}
