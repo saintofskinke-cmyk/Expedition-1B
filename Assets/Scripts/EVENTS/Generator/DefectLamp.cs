4fbdd4c9192fd2d506f0803682a9a7c9803db355
Simon Gade <simongade@duck.com>
2026-03-26 19:16:24 +0100

    Perfomance Update

--- Assets/Scripts/EVENTS/Generator/DefectLamp.cs
+++ Assets/Scripts/EVENTS/Generator/DefectLamp.cs
@@ -3,15 +3,18 @@ using UnityEngine;
 
 public class DefectLamp : MonoBehaviour
 {
+    GameObject bolbs;
+
     [Header("Bulb Settings")]
     [SerializeField] private float minOffTime = 0.5f;
     [SerializeField] private float maxOffTime = 1f;
     [SerializeField] private float minOnTime = 0.2f;
     [SerializeField] private float maxOnTime = 0.3f;
 
-    private void Start() { 
+    private void Start() {
+        bolbs = transform.GetChild(0).gameObject;
         EventManager.Generator += StartLights;
-        GetComponentInChildren<Light>().enabled = false;
+        bolbs.SetActive(false);
     }
    // private void OnDisable() { EventManager.Generator -= StartLights; }
     private void StartLights() { StartCoroutine(BlinkingLights()); }
@@ -20,12 +23,12 @@ public class DefectLamp : MonoBehaviour
     {
         while (true)
         {
-            GetComponentInChildren<Light>().enabled = false;
+            bolbs.SetActive(false);
             GetComponent<MeshRenderer>().material = GameManager.Instance.standardBulb_Unlit;
             float rndOffTime = Random.Range(minOffTime, maxOffTime);
             yield return new WaitForSeconds(rndOffTime);
 
-            GetComponentInChildren<Light>().enabled = true;
+            bolbs.SetActive(true);
             GetComponent<MeshRenderer>().material = GameManager.Instance.standardBulb_Lit;
             float rndOnTime = Random.Range(minOnTime, maxOnTime);
             yield return new WaitForSeconds(rndOnTime);
